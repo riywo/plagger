6a194c31cea035704e39025d0277b2c0c2f1cfc4
riywo <riywo.jp@gmail.com>
2009-01-07 02:26:57 +0900

    hatebu戻す

--- lib/Plagger/Plugin/Publish/HatenaBookmark.pm
+++ lib/Plagger/Plugin/Publish/HatenaBookmark.pm
@@ -1,4 +1,4 @@
-package Plagger::Plugin::Publish::HatenaBookmark2;
+package Plagger::Plugin::Publish::HatenaBookmark;
 use strict;
 use base qw( Plagger::Plugin );
 
@@ -65,7 +65,7 @@ __END__
 
 =head1 NAME
 
-Plagger::Plugin::Publish::HatenaBookmark2 - Post to Hatena::Bookmark automatically
+Plagger::Plugin::Publish::HatenaBookmark - Post to Hatena::Bookmark automatically
 
 =head1 SYNOPSIS
 
